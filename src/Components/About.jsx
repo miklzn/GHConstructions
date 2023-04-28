974a042f37bce39bd3ea8d351fbca3b377832025
miklzn <mikelzonsarabia@gmail.com>
2023-04-28 15:23:52 -0300

    About fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,18 +4,36 @@ const About = () => {
   return (
     <>
       <section className="h-auto">
-        <div className="px-[5vw] py-10 font-almarai  bg-emerald-900">
-          <p className="text-emerald-500 text-[1.125rem] mb-4">/ABOUT US</p>
-          <h1 className="text-[2.125rem] text-white font-semibold leading-10 min-[480px]:text-[2.5rem] md:pr-48">
-            A team of expert contractors ready to help you
-          </h1>
-          <p className="mt-7 font-catamaran text-white md:text-[1.125rem]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor sit
-            varius rhoncus, nullam pulvinar ut sollicitudin dictumst porttitor
-            porttitor quis quisque erat.
-          </p>
+        <div className="py-10 font-almarai  bg-emerald-900 md:py-14 md:w-full lg:px-[7vw] lg:py-12">
+          <div className="md:flex md:items-center">
+            <div className="px-[5vw] md:w-1/2 ">
+              <p className="text-emerald-500 text-[1.125rem] mb-4">/ABOUT US</p>
+              <h1 className="text-[2.125rem] text-white font-semibold leading-10 min-[480px]:text-[2.5rem]">
+                A team of expert contractors ready to help you
+              </h1>
+              <p className="mt-7 font-catamaran text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor
+                sit varius rhoncus, nullam pulvinar ut sollicitudin dictumst
+                porttitor porttitor quis quisque erat.
+              </p>
+            </div>
+            <div className="flex w-full justify-center space-x-4 px-[5vw] mt-7 md:w-1/2 md:mt-0">
+              <button
+                className="bg-white w-full py-2.5 px-6 text-sm text-gray-900 min-[480px]:w-full min-[480px]:text-base sm:w-full sm:py-[1.127rem] md:text-lg md:py-[1rem] md:px-9"
+                href="/"
+              >
+                Contact
+              </button>
+              <button
+                className="bg-emerald-500 w-full py-2.5 px-6 text-sm text-gray-900 min-[480px]:w-full min-[480px]:text-base sm:w-full sm:py-[1.127rem] md:text-lg md:py-[1rem] md:px-9"
+                href="/"
+              >
+                Services
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="px-[5vw] py-10 font-almarai">
+        <div className="px-[5vw] py-10 font-almarai lg:px-[12vw]">
           <div
             className="h-auto bg-cover bg-center"
             style={{
@@ -61,7 +79,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-[5vw] py-10 font-almarai ">
-          <div className="md:flex md:space-x-14 md:items-center">
+          <div className="md:flex md:space-x-14 md:items-center lg:px-[7vw]">
             <div className="md:w-1/2">
               <p className="text-emerald-500 text-[1.125rem] mb-4">
                 /OUR MISSION
@@ -85,7 +103,7 @@ const About = () => {
           </div>
         </div>
         <div className="px-[5vw] py-10 font-almarai">
-          <div>
+          <div className="lg:px-[7vw]">
             <p className="text-emerald-500 text-[1.125rem] mb-4">/OUR VALUES</p>
             <h1 className="text-[2rem] font-semibold leading-10 min-[480px]:text-[2.25rem]">
               The values that drive our work
@@ -96,7 +114,7 @@ const About = () => {
               porttitor quis quisque erat.
             </p>
           </div>
-          <div className="py-14 grid grid-cols-1 gap-6 md:grid-cols-3">
+          <div className="py-14 grid grid-cols-1 gap-6 md:grid-cols-3 lg:px-[7vw]">
             <div className="h-auto border px-6 py-10 text-center">
               <div className="flex justify-center mb-4 min-[480px]:mb-5">
                 <img className="w-14 min-[480px]:w-[70px]" src={medal} alt="" />
